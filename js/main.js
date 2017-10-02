e9eb400957e07002f50e40ebf257873525e05205
Arpit <banatiarpit97@yahoo.co.in>
2017-10-03 01:15:08 +0530

    sounds added;can play using mouse-click;play,pause,restart,resume button added;padding of bosy removed,now topOffset shows correct value;

--- js/main.js
+++ js/main.js
@@ -1,35 +1,102 @@
 // var a = $("#move").position();
     angular.module('snake',['ngMaterial'])
-    .controller('SnakeGame', function($scope){
-        $scope.mode = "die hitting walls";
+    .controller('SnakeGame', function($scope, $mdDialog){
         var score = 0;
-   
-var leftOffset = document.getElementById("boundary").offsetLeft;
+        var mode;
+        var speed;
+        var woah;
+        var frequency;
+        var leftOffset = document.getElementById("boundary").offsetLeft;
 var topOffset = document.getElementById("boundary").offsetTop;
 var width = document.getElementById("boundary").offsetWidth;
 var height = document.getElementById("boundary").offsetHeight;
-var ele = document.getElementById("boundary");
+var count = document.getElementById("boundary").childElementCount;
+var hit = document.getElementById("hit"); 
+var eat = document.getElementById("eat"); 
+var playedOnce;
+
+console.log(topOffset);
 
 var minX = leftOffset;
 var maxX = leftOffset+width;
-console.log(maxX, width)
-// var minY = topOffset;
-var minY = 60;
+var minY = topOffset;
 var maxY = topOffset+height;
-console.log(minX, maxX, minY, maxY);
 var u=".internal:nth-child("
 var r;
 var direction;
-var gameon=true;
-var count=ele.childElementCount;
-console.log(count);
-var left=100;
+var gameon=false;
+        $(".pause").hide();
+        $(".restart").hide();
+
+         $scope.mode = function(ev) {
+						 $mdDialog.show({
+								 controller: DialogController,
+								 templateUrl: 'gameModes.html',
+								 parent: angular.element(document.body),
+								 targetEvent: ev,
+								 clickOutsideToClose:true// Only for -xs, -sm breakpoints.
+						 });
+
+		 };
+// $(".playButton").click(function(){
+// 	if(playedOnce){
+// 		location.reload();
+// 	}
+// })		 
+
+
+
+		function DialogController($scope, $mdDialog) {
+
+            $scope.overflow = "die hitting walls";
+            $scope.speed = "medium";
+
+						 $scope.close = function() {
+								 $mdDialog.cancel();
+						 };
+						 $scope.cancel = function() {
+								 $mdDialog.cancel();
+						 };
+						 $scope.confirmMode = function() {
+               mode = $scope.overflow;
+               speed = $scope.speed;
+                   if(speed == "slow"){
+                       frequency = 200;
+                    } 
+                    else if(speed == "medium"){
+                      frequency = 100;
+                    }   
+                    else if(speed == "fast"){
+                      frequency = 50;
+                    } 
+                    console.log(speed, frequency)
+                    // startPosition();
+               woah = setInterval(right, frequency);
+               gameon = true;
+               direction ="right";
+               $(".playButton").hide();
+               $(".pause").show();
+               $(".restart").show();
+               $(".keyboard").css({"margin-top":"-47%", "margin-right":"-24%"});
+               $(".scorediv").css({"margin-top":"-27%", "margin-right":"-31%"});
+
+
+
+
+								 $mdDialog.cancel();
+						 };
+        };
+
+
+   
+
+var left=((count-1)*20);
+
 
 
 function generateFruit(){
 	randomFruitPosX = (Math.floor((Math.random() * ((maxX-20) - (minX+20))+(minX+20))/20)*20);
-    randomFruitPosY = (Math.floor((Math.random() * ((maxY-20) - (minY))+(minY))/20)*20)+5;
-    console.log(randomFruitPosX, randomFruitPosY);
+    randomFruitPosY = (Math.floor((Math.random() * ((maxY-20) - (minY+20))+(minY+20))/20)*20);
     $("#fruit").css("left", randomFruitPosX);
     $("#fruit").css("top", randomFruitPosY);
     $("#fruit").removeClass("hidden");
@@ -38,7 +105,8 @@ generateFruit();
 
 function checkContact(prevLeft, prevTop,s, j){
  // subtracted because of margin and borders
-	if((prevLeft == (randomFruitPosX-60)) && ((prevTop) == (randomFruitPosY-5))){
+	if((prevLeft == (randomFruitPosX-60)) && ((prevTop) == (randomFruitPosY-60))){
+		eat.play();
 		lastChild = ".internal:nth-child("+count+")";
 		lastChildX = $(lastChild).position().left-s;
 	    lastChildY = $(lastChild).position().top-j;
@@ -46,7 +114,6 @@ function checkContact(prevLeft, prevTop,s, j){
 		count++;
 		score++;
 		$("#score").html(score)
-		console.log(score)
 		lastChild = ".internal:nth-child("+count+")";
 		$(lastChild).css("left", lastChildX);
 		$(lastChild).css("top", lastChildY );
@@ -54,8 +121,72 @@ function checkContact(prevLeft, prevTop,s, j){
     }
 }
 
+$("#boundary").click(function(e){
+	clickX = (e.pageX-leftOffset-5);
+	clickY = (e.pageY-topOffset-5);
+	prevPosX = $(".internal:nth-child(1)").position().left;
+	prevPosY = $(".internal:nth-child(1)").position().top;
+	if((clickY>prevPosY) && (direction != "up")){
+		clearInterval(woah);
+		direction = "down";
+		woah=setInterval(down,frequency);
+	}
+	else if((clickY<prevPosY) && (direction != "down")){
+		clearInterval(woah);
+		direction = "up";
+		woah=setInterval(up,frequency);
+	}
+	else if((clickX>prevPosX) && (direction != "left")){
+		clearInterval(woah);
+		direction = "right";
+		woah=setInterval(right,frequency);
+	}
+	else if((clickX<prevPosX) && (direction != "right")){
+		clearInterval(woah);
+		direction = "left";
+		woah=setInterval(lef,frequency);
+	}
 
- 
+
+})
+
+$(".playButton").click(function(){
+	if(playedOnce){
+		location.reload();
+	}
+})
+
+$(".restart").click(function(){
+  location.reload();
+})
+$(".pause").click(function(){
+  if(gameon){
+    clearInterval(woah);
+    gameon = false;
+    $(this).html("Resume");
+  }
+  else{
+    if(direction == "right"){
+       woah=setInterval(right,frequency);
+    }
+    else if(direction == "left"){
+       woah=setInterval(lef,frequency);
+    }
+    else if(direction == "up"){
+       woah=setInterval(up,frequency);
+    }
+    else if(direction == "down"){
+       woah=setInterval(down,frequency);
+    }
+    gameon = true;
+    $(this).html("Pause");
+  } 
+
+});
+
+
+
+// to reverse the order of internal div 
 for(var i=1;i<=count;i++)
 {
 	r=u+i+")";
@@ -81,33 +212,77 @@ function right(){
 	move(20,0);
 };
 
-var woah = setInterval(right, 100);
 
 $(document).keypress(function(e){
 	if((String.fromCharCode(e.which) == "s") && (gameon) && (direction!="up")){
 		clearInterval(woah);
 		direction = "down";
-
-		woah=setInterval(down,100);
+		woah=setInterval(down,frequency);
+		$(".skey").css("background-color", "grey");
+		setTimeout(function(){$(".skey").css("background-color", "#e0dcdc")}, 300)
 	}
 	else if((String.fromCharCode(e.which)=="d") && (gameon) && (direction!="left")){
 		clearInterval(woah);
 		direction = "right";
-
-		woah=setInterval(right,100);
+		woah=setInterval(right,frequency);
+		$(".dkey").css("background-color", "grey");
+		setTimeout(function(){$(".dkey").css("background-color", "#e0dcdc")}, 300)
 	}
 	else if((String.fromCharCode(e.which)=="a") && (gameon) && (direction!="right")){
 		clearInterval(woah);
 		direction = "left";
-
-		woah=setInterval(lef,100);
+		woah=setInterval(lef,frequency);
+		$(".akey").css("background-color", "grey");
+		setTimeout(function(){$(".akey").css("background-color", "#e0dcdc")}, 300)
 	}
 	else if((String.fromCharCode(e.which)=="w") && (gameon) && (direction!="down")){
 		clearInterval(woah);
 		direction = "up";
-		woah=setInterval(up,100);
+		woah=setInterval(up,frequency);
+		$(".wkey").css("background-color", "grey");
+		setTimeout(function(){$(".wkey").css("background-color", "#e0dcdc")}, 300)
 	}
 });
+
+	$(".skey").click(function(){
+		if((gameon) && (direction!="up")){
+			clearInterval(woah);
+			direction = "down";
+			woah=setInterval(down,frequency);
+			$(".skey").css("background-color", "grey");
+			setTimeout(function(){$(".skey").css("background-color", "#e0dcdc")}, 300)
+	    }
+	});
+
+	$(".dkey").click(function(){
+		if((gameon) && (direction!="left")){
+			clearInterval(woah);
+		direction = "right";
+		woah=setInterval(right,frequency);
+		$(".dkey").css("background-color", "grey");
+		setTimeout(function(){$(".dkey").css("background-color", "#e0dcdc")}, 300)
+	    }
+	});
+
+	$(".akey").click(function(){
+		if((gameon) && (direction!="right")){
+		clearInterval(woah);
+		direction = "left";
+		woah=setInterval(lef,frequency);
+		$(".akey").css("background-color", "grey");
+		setTimeout(function(){$(".akey").css("background-color", "#e0dcdc")}, 300)
+	    }
+	});
+	
+		$(".wkey").click(function(){
+		if((gameon) && (direction!="down")){
+			clearInterval(woah);
+		direction = "up";
+		woah=setInterval(up,frequency);
+		$(".wkey").css("background-color", "grey");
+		setTimeout(function(){$(".wkey").css("background-color", "#e0dcdc")}, 300)
+	    }
+	});	
 
 
 function move(s,j)
@@ -120,7 +295,7 @@ z = hitItself(prevLeft, prevTop);
 if(z==1){
 	return;
 }
-if($scope.mode == "die hitting walls"){
+if(mode == "die hitting walls"){
 	y = hitWall(prevLeft, prevTop);
 	if(y==1){
 		return;
@@ -158,17 +333,22 @@ function hitItself(prevLeft, prevTop){
 	for(i=2;i<=count;i++){
 		r=u+i+")";
 		if(($(r).position().left == prevLeft) && ($(r).position().top==prevTop)){
+            hit.play();
 			clearInterval(woah);
 			gameon = false;
+			gameEnd();
 			return "1";
 		}
 	}
 }
 
 function hitWall(prevLeft, prevTop){
-   if((prevLeft >= width)||(prevLeft <= -35)||(prevTop >= height)||(prevTop == -40)){
+	// -20 to stop at the boundary only, not go ahead
+   if((prevLeft >= (width-20))||(prevLeft <= -15)||(prevTop >= (height-20))||(prevTop == -20)){
    	  clearInterval(woah);
 	  gameon=false;
+      hit.play();
+      gameEnd();
 	  return "1";
    }
 }
@@ -188,3 +368,28 @@ function throughWall(prevLeft, prevTop){
    }
 }
  })
+
+function gameEnd(){
+    $(".pause").hide();
+    $(".restart").hide();
+	$(".playButton").show();
+	$(".keyboard").css({"margin-top":"-54%", "margin-right":"-45%"});
+    $(".scorediv").css({"margin-top":"-31%", "margin-right":"-51%"});
+    playedOnce = true;
+}
+
+// function startPosition(){
+// var count = 6;
+// var left=((count-1)*20);
+// //to reverse the order of internal div 
+// for(var i=1;i<=count;i++)
+// {
+//   r=u+i+")";
+//   $(r).css("left",left);
+//   left=left-20;
+// }
+// score = 0;
+// $("#score").html(score);
+// direction = right;
+
+// }
